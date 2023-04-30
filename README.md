9ef23582ba6e6742c1d28f2124ab2d74bcba23cb
Eugene <eugenevdm@gmail.com>
2023-04-30 08:00:17 +0200

    add a few more examples

--- README.md
+++ README.md
@@ -1,27 +1,48 @@
-# Rust
-A couple of introductory Rust examples
+# Rust Demo Applications
+An introduction to Rust.
 
 [![Rust build](https://github.com/eugenevdm/rust/actions/workflows/rust.yml/badge.svg)](https://github.com/eugenevdm/rust/actions/workflows/rust.yml)
 
 ## About
 
-This is a demo application to showcase various Rust features.
+Demo applications that showcases various Rust features.
 
-- Print Hello World
+## Installation
+
+For the [Systemd](https://https://en.wikipedia.org/wiki/Systemd) utilities to work, do this:
+`sudo apt-get install libsystemd-dev`
+
+### Main Showcase
+
+- Prints Hello World
+- Reads a MySQL `users` table
+- Get a Linux service status
+- Prints a directory listing
+- Use `Systemd` to restart a Linux service
+- Get the size of a folder
+- Get total disk size and available space left on the disk
 - Write a text file to disk
 - Read a text file from disk
 - Find and replace text in a text file from disk
 - Create a line on a surface and save it to a file using the Cairo library
-- Draw some text characters using SINE and COSINE functions
-- Create a menu and handle keyboard input
-- Extremely rudimentary unit test based on output from ChatGPT
-- Sprite (from https://github.com/PistonDevelopers/piston-examples)
+- Draw some text characters using [sine and cosine](https://en.wikipedia.org/wiki/Sine_and_cosine) functions
+- Create a console menu and handle keyboard input
+- A rudimentary unit test
 
-## Running
+#### Running the Main Demo
 
+`cargo run` or
 `cargo build;./target/debug/hello_world_rust`
 
-### Sprite
+Build Release:
+
+`cargo build --release`
+
+### Seperate Demos
+
+- Sprite (from https://github.com/PistonDevelopers/piston-examples)
+
+#### Running the Sprite demo
 
 `cargo run --example sprite`
 
--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,135 @@ use std::io::Write; // For writing to a file
 use std::fs; // Used to read a file
 use std::fs::{OpenOptions}; // Used by file search and replace
 use std::io::{BufRead, BufReader}; // Also used by file search and replace
+use fs2::{free_space, total_space}; // Used to get disk space
+use std::path::Path; // Used to get disk space
+use std::process::Command;
+use regex::Regex; // Used for regular expressions
+use mysql::{Pool};
+use mysql::prelude::*;
 
-/*  
-    Main entry point into the application.
-*/
-fn main() -> io::Result<()>{
-    println!("Hello, world!");
+#[derive(Debug, PartialEq, Eq)]
+struct User {
+    id: i32,
+    name: Option<String>,
+    email: Option<String>,
+}
 
-    write_text_file();
+fn read_mysql_table() -> std::result::Result<(), Box<dyn std::error::Error>> {    
+    let url = "mysql://root:@localhost:3306/blog";
+    let pool = Pool::new(url)?;
 
-    print_file_contents("vhost.example.conf")?;
+    let mut conn = pool.get_conn()?;
+
+    // Let's select payments from database. Type inference should do the trick here.
+    let selected_users = conn
+        .query_map(
+            "SELECT id, name, email FROM users",
+            |(id, name, email)| {
+                User { id, name, email }
+            },
+        )?;
+
+    println!("Yay!");
+
+    println!("Found {:?} users", selected_users.len());
+
+    for user in selected_users {
+        println!("Found user {:?}", user);
+    }
+
+    Ok(())
     
-    wait();
+}
 
-    let search = "129.232.252.163";
-    let replace = "1.1.1.1";
-    let filename = "vhost.example.conf";
+fn linux_service_status() {
+    let output = Command::new("systemctl")
+                    .arg("status")
+                    .arg("nginx")
+                     .output()
+                     .expect("Failed to execute command");
 
-    replace_file_content(search, replace, filename);
+    let text_output = String::from_utf8_lossy(&output.stdout);
 
-    print_file_contents("vhost.example.conf")?;
-
-    line();
-
-    wait();
-
-    sine_cosine();
-
-    println!("Above is the SINE/COSINE graph.");
-
-    println!("Next there will be a menu.");
-
-    wait();
-
-    menu();
-
-    Ok(()) // Return value required by main function
+    let re = Regex::new(r"Active: active").unwrap();
     
+    if re.is_match(&text_output) {
+        println!("Nginx is running");
+    } else {
+        println!("Nginx is not running");
+    }    
+}
+
+fn get_directory_listing() {
+    let output = Command::new("ls")
+                    .arg("-lah")
+                     .output()
+                     .expect("Failed to execute command");
+
+    println!("Output {}", String::from_utf8_lossy(&output.stdout));
+}
+
+fn restart_linux_service() {
+    let service_name = "nginx"; // Replace with your service name
+
+    let mut systemctl = Command::new("systemctl");
+    
+    systemctl.arg("restart").arg(service_name);
+
+    let output = match systemctl.output() {
+        Ok(output) => output,
+        Err(e) => {
+            println!("Error occurred: {}", e);            
+            return;
+        }
+    };
+
+    if output.status.success() {
+        println!("Success {}", output.status);        
+    } else {
+        println!("Failed {}", output.status);        
+    }
+}
+
+fn read_disk_space() {
+    let path = Path::new("/home/eugene/code/hello_world_rust/assets");
+
+    let size = get_directory_size(path).unwrap();
+
+    println!("Size of {:?}: {} bytes", path, size);
+
+    wait();
+}
+
+/*
+    Good example of an recursive function that calls itself
+ */
+fn get_directory_size(path: &Path) -> std::io::Result<u64> {
+    let mut size = 0;
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let entry_path = entry.path();
+
+        if entry_path.is_file() {
+            size += entry.metadata()?.len();
+        } else {
+            size += get_directory_size(&entry_path)?;
+        }
+    }
+
+    Ok(size)
+}
+
+fn get_total_disk_size()  {    
+    let path = "/home/eugene/code/hello_world_rust";
+    
+    let total_space = total_space(path).unwrap();
+    let free_space = free_space(path).unwrap();
+    let used_space = total_space - free_space;
+
+    println!("Total space: {} bytes", total_space);
+    println!("Free space: {} bytes", free_space);
+    println!("Used space: {} bytes", used_space);
 }
 
 fn replace_file_content(search: &str, replace: &str, filename: &str)  {
@@ -186,4 +278,59 @@ fn wait() {
     println!("Press enter to continue...");
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
+}
+
+/*  
+    Main entry point into the application.
+*/
+fn main() -> io::Result<()>{
+    println!("Hello, world!");
+
+    let result = read_mysql_table();
+    assert!(result.is_ok());
+
+    wait();
+
+    linux_service_status();
+
+    wait();
+
+    get_directory_listing();
+
+    restart_linux_service();
+
+    read_disk_space();
+
+    get_total_disk_size();
+
+    write_text_file();
+
+    print_file_contents("vhost.example.conf")?;
+    
+    wait();
+
+    let search = "129.232.252.163";
+    let replace = "1.1.1.1";
+    let filename = "vhost.example.conf";
+
+    replace_file_content(search, replace, filename);
+
+    print_file_contents("vhost.example.conf")?;
+
+    line();
+
+    wait();
+
+    sine_cosine();
+
+    println!("Above is the SINE/COSINE graph.");
+
+    println!("Next there will be a menu.");
+
+    wait();
+
+    menu();
+
+    Ok(()) // Return value required by main function
+    
 }
