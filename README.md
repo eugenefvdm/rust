8aa725ca70489f37ab04f19e617ad31c3ec20274
Eugene <eugenevdm@gmail.com>
2023-04-30 12:15:21 +0200

    fix dev dependency

--- README.md
+++ README.md
@@ -15,30 +15,43 @@ For the [Systemd](https://https://en.wikipedia.org/wiki/Systemd) utilities to wo
 ### Main Showcase
 
 - Prints Hello World
-- Reads a MySQL `users` table
-- Get a Linux service status
+- Reads a `MySQL` table called `users`
+- Gets nginx service status
 - Prints a directory listing
-- Use `Systemd` to restart a Linux service
-- Get the size of a folder
+- Get the size of a directory
 - Get total disk size and available space left on the disk
 - Write a text file to disk
 - Read a text file from disk
-- Find and replace text in a text file from disk
-- Create a line on a surface and save it to a file using the Cairo library
+- Find and replace text in a text file on disk
+- Draw a line on a surface and save it to a file using the Cairo library
 - Draw some text characters using [sine and cosine](https://en.wikipedia.org/wiki/Sine_and_cosine) functions
+- Use `Systemd` to restart a Linux service
 - Create a console menu and handle keyboard input
 - A rudimentary unit test
 
 #### Running the Main Demo
 
-`cargo run` or
-`cargo build;./target/debug/hello_world_rust`
+```bash
+cargo run
+```
 
-Build Release:
+or
 
-`cargo build --release`
+```bash
+cargo build
+./target/debug/hello_world_rust
+```
 
-### Seperate Demos
+or
+
+```bash
+cargo build --release
+./target/release/hello_world_rust
+```
+
+### Running the Sprite Demo
+
+This demo is in the examples directory.
 
 - Sprite (from https://github.com/PistonDevelopers/piston-examples)
 
--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,48 @@
 extern crate cairo;
 
 use cairo::{Context, Format, ImageSurface}; // Import the Cairo library for line drawing on a surface
-use std::fs::File; // Import the File library for writing to a file
-use std::io; // Import the IO library for reading and writing to the console
-use std::io::Write; // For writing to a file
-use std::fs; // Used to read a file
-use std::fs::{OpenOptions}; // Used by file search and replace
-use std::io::{BufRead, BufReader}; // Also used by file search and replace
-use fs2::{free_space, total_space}; // Used to get disk space
-use std::path::Path; // Used to get disk space
-use std::process::Command;
-use regex::Regex; // Used for regular expressions
-use mysql::{Pool};
+use fs2::{free_space, total_space}; // Disk space utilities
+use mysql::Pool;
 use mysql::prelude::*;
+use std::fs::File; // Writing to a file
+use std::io; // Reading and writing to the console
+use std::io::Write; // Writing to a file
+use std::fs; // Read a file
+use std::fs::OpenOptions; // File search and replace
+use std::io::{BufRead, BufReader}; // File search and replace
+use std::path::Path;
+use std::process::Command;
+use regex::Regex;
 
-#[derive(Debug, PartialEq, Eq)]
-struct User {
-    id: i32,
-    name: Option<String>,
-    email: Option<String>,
-}
-
-fn read_mysql_table() -> std::result::Result<(), Box<dyn std::error::Error>> {    
-    let url = "mysql://root:@localhost:3306/blog";
-    let pool = Pool::new(url)?;
-
-    let mut conn = pool.get_conn()?;
-
-    // Let's select payments from database. Type inference should do the trick here.
-    let selected_users = conn
-        .query_map(
-            "SELECT id, name, email FROM users",
-            |(id, name, email)| {
-                User { id, name, email }
-            },
-        )?;
-
-    println!("Yay!");
-
-    println!("Found {:?} users", selected_users.len());
-
-    for user in selected_users {
-        println!("Found user {:?}", user);
-    }
-
-    Ok(())
-    
-}
-
-fn linux_service_status() {
-    let output = Command::new("systemctl")
-                    .arg("status")
-                    .arg("nginx")
-                     .output()
-                     .expect("Failed to execute command");
-
-    let text_output = String::from_utf8_lossy(&output.stdout);
-
-    let re = Regex::new(r"Active: active").unwrap();
-    
-    if re.is_match(&text_output) {
-        println!("Nginx is running");
-    } else {
-        println!("Nginx is not running");
-    }    
-}
-
-fn get_directory_listing() {
+fn directory_listing() {
     let output = Command::new("ls")
                     .arg("-lah")
                      .output()
                      .expect("Failed to execute command");
 
-    println!("Output {}", String::from_utf8_lossy(&output.stdout));
+    println!("Directory listing Output:\n{}", String::from_utf8_lossy(&output.stdout));
 }
 
-fn restart_linux_service() {
-    let service_name = "nginx"; // Replace with your service name
+fn draw_line_on_surface() {
+    let surface_width = 300;
+    let surface_height = 200;
 
-    let mut systemctl = Command::new("systemctl");
-    
-    systemctl.arg("restart").arg(service_name);
+    let surface = ImageSurface::create(Format::ARgb32, surface_width, surface_height).unwrap();
+    let context = Context::new(&surface);
 
-    let output = match systemctl.output() {
-        Ok(output) => output,
-        Err(e) => {
-            println!("Error occurred: {}", e);            
-            return;
-        }
-    };
+    context.set_line_width(10.0);
+    context.move_to(0.0, surface_height as f64 / 2.0);
+    context.line_to(surface_width as f64, surface_height as f64 / 2.0);
+    context.stroke();
 
-    if output.status.success() {
-        println!("Success {}", output.status);        
-    } else {
-        println!("Failed {}", output.status);        
-    }
-}
+    let mut file = File::create("line.png").unwrap();
+    surface.write_to_png(&mut file).unwrap();
 
-fn read_disk_space() {
-    let path = Path::new("/home/eugene/code/hello_world_rust/assets");
-
-    let size = get_directory_size(path).unwrap();
-
-    println!("Size of {:?}: {} bytes", path, size);
-
-    wait();
+    println!("A file line.png has been created.");
 }
 
 /*
-    Good example of an recursive function that calls itself
+    Good example of a recursive function that calls itself
  */
 fn get_directory_size(path: &Path) -> std::io::Result<u64> {
     let mut size = 0;
@@ -138,91 +72,43 @@ fn get_total_disk_size()  {
     println!("Used space: {} bytes", used_space);
 }
 
-fn replace_file_content(search: &str, replace: &str, filename: &str)  {
-    let file = File::open(filename).expect("Could not open file");
-    let reader = BufReader::new(file);
-    let mut lines = reader.lines();
+fn linux_service_restart() {
+    let service_name = "nginx"; // Replace with your service name
 
-    let mut new_content = String::new();
+    let mut systemctl = Command::new("systemctl");
+    
+    systemctl.arg("restart").arg(service_name);
 
-    while let Some(line) = lines.next() {
-        let line = line.unwrap();
-        let new_line = line.replace(search, replace);
-        new_content.push_str(&new_line);
-        new_content.push('\n');
-    }
+    let output = match systemctl.output() {
+        Ok(output) => output,
+        Err(e) => {
+            println!("Error occurred: {}", e);            
+            return;
+        }
+    };
 
-    let mut file = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .open(filename)
-        .expect("Could not open file for writing");
-
-    file.write_all(new_content.as_bytes())
-        .expect("Could not write to file");
-}
-
-fn write_text_file() {
-    let mut file = File::create("vhost.example.conf").unwrap();
-
-    let indentation = "    "; // 4 spaces
-
-    write!(file, "<VirtualHost 129.232.252.163:80>\n").unwrap();
-    write!(file, "{}SuexecUserGroup \"#1356\" \"#1056\"\n", indentation).unwrap();
-    write!(file, "{}ServerName example.com\n", indentation).unwrap();
-    write!(file, "{}ServerAlias www.example.com\n", indentation).unwrap();
-    write!(file, "{}ServerAlias webmail.example.com\n", indentation).unwrap();  
-}
-
-fn print_file_contents(filename: &str) -> io::Result<()> {
-    let content = fs::read_to_string(filename)?;
-
-    println!("{}", content);
-
-    Ok(())
-}
-
-fn line() {
-    let surface_width = 300;
-    let surface_height = 200;
-
-    let surface = ImageSurface::create(Format::ARgb32, surface_width, surface_height).unwrap();
-    let context = Context::new(&surface);
-
-    context.set_line_width(10.0);
-    context.move_to(0.0, surface_height as f64 / 2.0);
-    context.line_to(surface_width as f64, surface_height as f64 / 2.0);
-    context.stroke();
-
-    let mut file = File::create("line.png").unwrap();
-    surface.write_to_png(&mut file).unwrap();
-
-    println!("A file line.png has been created.");
-}
-
-// Sine Consine example variables    
-const WIDTH: i32 = 100;
-const HEIGHT: i32 = 50;
-
-fn sine_cosine() {
-    let mut canvas = vec![vec![' '; WIDTH as usize]; HEIGHT as usize];
-
-    for x in 0..WIDTH {
-        let y = (HEIGHT as f64 / 2.0 - (HEIGHT as f64 / 2.0 * (x as f64 / WIDTH as f64).sin())) as i32;
-        canvas[y as usize][x as usize] = '*';
-    }
-
-    for y in 0..HEIGHT {
-        let x = (WIDTH as f64 / 2.0 + (HEIGHT as f64 / 2.0 * (y as f64 / HEIGHT as f64).cos())) as i32;
-        canvas[y as usize][x as usize] = '*';
-    }
-
-    for row in canvas {
-        println!("{}", row.into_iter().collect::<String>());
+    if output.status.success() {
+        println!("Success {}", output.status);        
+    } else {
+        println!("Failed {}", output.status);        
     }
 }
 
-
+fn linux_service_status() {
+    let output = Command::new("systemctl")
+                    .arg("status")
+                    .arg("nginx")
+                    .output()
+                    .expect("Failed to execute command");
+    let text_output = String::from_utf8_lossy(&output.stdout);
+    let re = Regex::new(r"Active: active").unwrap();
+    
+    if re.is_match(&text_output) {
+        println!("Nginx is running");
+    } else {
+        println!("Nginx is not running");
+    }    
+}
 
 fn menu() {
     let menu_items = vec!["Option 1", "Option 2", "Option 3", "Quit (q)"];
@@ -274,6 +160,114 @@ fn menu() {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+struct User {
+    id: i32,
+    name: Option<String>,
+    email: Option<String>,
+}
+
+fn mysql_read_users_table() -> std::result::Result<(), Box<dyn std::error::Error>> {    
+    let url = "mysql://root:@localhost:3306/blog";
+    let pool = Pool::new(url)?;
+
+    let mut conn = pool.get_conn()?;
+
+    // Let's select payments from database. Type inference should do the trick here.
+    let selected_users = conn
+        .query_map(
+            "SELECT id, name, email FROM users",
+            |(id, name, email)| {
+                User { id, name, email }
+            },
+        )?;
+
+    println!("Yay! Found {:?} MySQL users:", selected_users.len());
+
+    for user in selected_users {
+        println!(" {:?}", user);
+    }
+
+    Ok(())
+    
+}
+
+fn print_file_contents(filename: &str) -> io::Result<()> {
+    let content = fs::read_to_string(filename)?;
+
+    println!("{}", content);
+
+    Ok(())
+}
+
+fn read_disk_space(path: &str) -> std::io::Result<u64> {
+    let path = Path::new(path);
+
+    let size = get_directory_size(path).unwrap();
+
+    //println!("Size of {:?}: {} bytes", path, size);
+
+    Ok(size)
+}
+
+fn replace_file_content(search: &str, replace: &str, filename: &str)  {
+    let file = File::open(filename).expect("Could not open file");
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+
+    let mut new_content = String::new();
+
+    while let Some(line) = lines.next() {
+        let line = line.unwrap();
+        let new_line = line.replace(search, replace);
+        new_content.push_str(&new_line);
+        new_content.push('\n');
+    }
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(filename)
+        .expect("Could not open file for writing");
+
+    file.write_all(new_content.as_bytes())
+        .expect("Could not write to file");
+}
+
+// Sine Consine example variables    
+const WIDTH: i32 = 100;
+const HEIGHT: i32 = 50;
+
+fn sine_cosine() {
+    let mut canvas = vec![vec![' '; WIDTH as usize]; HEIGHT as usize];
+
+    for x in 0..WIDTH {
+        let y = (HEIGHT as f64 / 2.0 - (HEIGHT as f64 / 2.0 * (x as f64 / WIDTH as f64).sin())) as i32;
+        canvas[y as usize][x as usize] = '*';
+    }
+
+    for y in 0..HEIGHT {
+        let x = (WIDTH as f64 / 2.0 + (HEIGHT as f64 / 2.0 * (y as f64 / HEIGHT as f64).cos())) as i32;
+        canvas[y as usize][x as usize] = '*';
+    }
+
+    for row in canvas {
+        println!("{}", row.into_iter().collect::<String>());
+    }
+}
+
+fn text_file_creation() {
+    let mut file = File::create("vhost.example.conf").unwrap();
+
+    let indentation = "    "; // 4 spaces
+
+    write!(file, "<VirtualHost 129.232.252.163:80>\n").unwrap();
+    write!(file, "{}SuexecUserGroup \"#1356\" \"#1056\"\n", indentation).unwrap();
+    write!(file, "{}ServerName example.com\n", indentation).unwrap();
+    write!(file, "{}ServerAlias www.example.com\n", indentation).unwrap();
+    write!(file, "{}ServerAlias webmail.example.com\n", indentation).unwrap();  
+}
+
 fn wait() {
     println!("Press enter to continue...");
     let mut input = String::new();
@@ -281,12 +275,12 @@ fn wait() {
 }
 
 /*  
-    Main entry point into the application.
+    Main entry point to the application.
 */
 fn main() -> io::Result<()>{
     println!("Hello, world!");
 
-    let result = read_mysql_table();
+    let result = mysql_read_users_table();
     assert!(result.is_ok());
 
     wait();
@@ -295,40 +289,40 @@ fn main() -> io::Result<()>{
 
     wait();
 
-    get_directory_listing();
-
-    restart_linux_service();
-
-    read_disk_space();
+    directory_listing();
+    println!("Here is `ls -la` output ^^^");
+        
+    println!("Next going to output the size of a directory:");  
+    let path = "/home/eugene/code/hello_world_rust/assets";
+    let result = read_disk_space(path);    
+    println!("Size of {:?} is {} bytes", path, result.unwrap());    
+    wait();    
 
     get_total_disk_size();
-
-    write_text_file();
-
-    print_file_contents("vhost.example.conf")?;
-    
-    wait();
-
+    text_file_creation();
+            
     let search = "129.232.252.163";
     let replace = "1.1.1.1";
     let filename = "vhost.example.conf";
 
     replace_file_content(search, replace, filename);
-
     print_file_contents("vhost.example.conf")?;
+    println!("A file was created and then search and replace took place ^^^");
 
-    line();
+    draw_line_on_surface();    
 
     wait();
 
-    sine_cosine();
+    sine_cosine();    
+    println!("Above is the sine and consine graph.");
 
-    println!("Above is the SINE/COSINE graph.");
+    println!("Next going to restart nginx. You can press escape to cancel.");
+    wait();
+
+    linux_service_restart();
 
     println!("Next there will be a menu.");
-
-    wait();
-
+    wait();    
     menu();
 
     Ok(()) // Return value required by main function
